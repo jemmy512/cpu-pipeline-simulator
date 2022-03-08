0fa27b216a5d75e21da554e6a93774868a3dba9c
Jemmy <jemmy1029760725@gmail.com>
2022-03-09 01:17:05 +0800

    Resolve contorl hazard (#3)

--- project1/CPU.c
+++ project1/CPU.c
@@ -8,6 +8,7 @@
 #include <assert.h>
 #include "CPU.h"
 #include "trace.h"
+#include <stdio.h>
 
 unsigned int cycle_number = 0;
 unsigned int inst_number = 0;
@@ -19,8 +20,9 @@ dynamic_inst EX_lwsw = {0}, MEM_lwsw = {0};
 bool WB_ctrl = 1;
 bool MEM_ctrl[2] = {1, 1};
 bool EX_ctrl[3] = {1, 1, 1};
-bool ID_ctrl = 1;
-bool IF_ctrl = 1;
+bool Ctrl_ID = 1;
+bool Ctrl_IF = 1;
+bool Ctrl_branch_taken = 0;
 
 bool is_ALU(dynamic_inst dinst) {
   instruction inst = dinst.inst;
@@ -40,6 +42,10 @@ bool is_lw(dynamic_inst dinst) {
 bool is_NOP(dynamic_inst dinst) {
   instruction inst = dinst.inst;
   return inst.type == ti_NOP;
+}
+
+bool is_branch(dynamic_inst dinst) {
+  return dinst.inst.type == ti_BRANCH;
 }
 
 bool is_older(dynamic_inst dinst1, dynamic_inst dinst2) {
@@ -130,7 +136,7 @@ bool is_finished()
 //   }
 
 //   if (verbose) {/* print the instruction exiting the pipeline if verbose=1 */
-//     for (int i = 0; i < (int) WB.size(); i++) {
+//     for (auto i = 0; i < (int) WB.size(); i++) {
 //       printf("[%d: WB] %s\n", cycle_number, get_instruction_string(WB[i], true));
 //       if(!is_NOP(WB[i])) {
 //         if(config->pipelineWidth > 1 && config->regFileWritePorts == 1) {
@@ -152,19 +158,20 @@ int writeback()
   static unsigned int cur_seq = 1;
   bool continueWB = true;
 
+  // J: data_raw 2
   bool doOp = false;
-  for (int i = 0; i < WB.size(); ++i) {
+  for (auto i = 0; i < WB.size(); ++i) {
     doOp = doOp || !is_NOP(WB[i]);
   }
 
   if (doOp) {
-    IF_ctrl = true;
-    ID_ctrl = true;
+    Ctrl_IF = true;
+    Ctrl_ID = true;
   }
 
   WB.clear();
 
-  // J: structual hazard register write
+  // J: structural_regfile
   if (is_ALU(MEM_ALU) && is_lw(MEM_lwsw) && (config->regFileWritePorts == 1)) {
     continueWB = false;
 
@@ -177,7 +184,7 @@ int writeback()
     }
   }
 
-  // J: WAW
+  // J: data_waw
   if (continueWB && config->regFileWritePorts == 2) {
     if (is_ALU(MEM_ALU) && is_lw(MEM_lwsw) && MEM_ALU.inst.dReg == MEM_lwsw.inst.dReg) {
       continueWB = false;
@@ -207,7 +214,7 @@ int writeback()
   }
 
   if (verbose) {/* print the instruction exiting the pipeline if verbose=1 */
-    for (int i = 0; i < (int) WB.size(); i++) {
+    for (auto i = 0; i < (int) WB.size(); i++) {
       printf("[%d: WB] %s\n", cycle_number, get_instruction_string(WB[i], true));
       if(!is_NOP(WB[i])) {
         if(config->pipelineWidth > 1 && config->regFileWritePorts == 1) {
@@ -227,6 +234,10 @@ int writeback()
 
 int memory()
 {
+  if (is_branch(EX_ALU)) {
+    Ctrl_branch_taken = false;
+  }
+
   int insts = 0;
   if (is_NOP(MEM_ALU)) {
     MEM_ALU = EX_ALU;
@@ -240,7 +251,7 @@ int memory()
     insts++;
   }
 
-  // J: structual memory read hazard
+  // J: structural_memory
   // if (config->splitCaches == false && is_lw(MEM_lwsw)) {
   //   int i = IF.size() - 1;
   //   int nopCount = 0;
@@ -288,6 +299,52 @@ int issue()
     }
     insts++;
   }
+
+  // J: control_not_taken
+  // J: control_taken
+  // if (is_branch(EX_ALU)) {
+  //   bool isBranchTaken = true;
+  //   int addr = EX_ALU.inst.Addr;
+
+  //   if (!is_NOP(EX_lwsw)) {
+  //     printf("LW %d | %d\n", addr, EX_lwsw.inst.PC);
+  //     if (addr == EX_lwsw.inst.PC) {
+  //       isBranchTaken = false;
+  //     }
+  //   }
+
+  //   for (auto i = 0; i < ID.size(); ++i) {
+  //     printf("ID %d | %d\n", addr, ID[i].inst.PC);
+  //     if (addr == ID[i].inst.PC) {
+  //       isBranchTaken = false;
+  //     }
+  //   }
+
+  //   for (auto i = 0; i < IF.size(); ++i) {
+  //     printf("IF %d | %d\n", addr, IF[i].inst.PC);
+  //     if (addr == IF[i].inst.PC) {
+  //       isBranchTaken = false;
+  //     }
+  //   }
+
+  //   if (isBranchTaken) {
+  //     printf("branch taken...\n");
+
+  //     insts = 0;
+  //     for (auto i = 0; i < ID.size(); ++i) {
+  //       IF.push_front(ID.back());
+  //       ID.pop_back();
+  //     }
+
+  //     for (auto i = 0; i < config->pipelineWidth; ++i) {
+  //       IF.push_front(get_NOP());
+  //     }
+  //   } else {
+  //     printf("branch not taken...\n");
+  //   }
+
+  // }
+
   return insts;
 }
 
@@ -322,18 +379,18 @@ int issue()
 int decode()
 {
   int insts = 0;
-  while (ID_ctrl && (int)IF.size() > 0 && (int)ID.size() < config->pipelineWidth) {
+  while (Ctrl_ID && (int)IF.size() > 0 && (int)ID.size() < config->pipelineWidth) {
     ID.push_back(IF.front());
     IF.pop_front();
     insts++;
   }
 
-  // J: RAW
-  if (ID_ctrl && config->enableForwarding == false) {
+  // J: data_raw 1
+  if (Ctrl_ID && config->enableForwarding == false) {
     int alu_dReg = EX_ALU.inst.dReg;
     int ls_dReg = EX_lwsw.inst.dReg;
 
-    for (int i = 0; i < ID.size(); ++i) {
+    for (auto i = 0; i < ID.size(); ++i) {
       int reg_a = ID[i].inst.sReg_a;
       int reg_b = ID[i].inst.sReg_b;
 
@@ -341,17 +398,17 @@ int decode()
       bool isLWRAW = is_lw(EX_lwsw) && (ls_dReg == reg_a || ls_dReg == reg_b);
 
       if (isALURAW || isLWRAW) {
-        for (int j = ID.size() - 1; j >= i; --j) {
+        for (auto j = ID.size() - 1; j >= i; --j) {
           insts--;
           IF.push_front(ID.back());
           ID.pop_back();
         }
 
-        IF_ctrl = false;
-        ID_ctrl = false;
+        Ctrl_IF = false;
+        Ctrl_ID = false;
       } else {
-        IF_ctrl = true;
-        ID_ctrl = true;
+        Ctrl_IF = true;
+        Ctrl_ID = true;
       }
     }
   }
@@ -367,20 +424,42 @@ int fetch()
   instruction *tr_entry = NULL;
 
   /* copy trace entry(s) into IF stage */
-  while (IF_ctrl && (int)IF.size() < config->pipelineWidth) {
+  auto doFetch = []() {
+    if (!Ctrl_branch_taken)
+      return true;
+
+    if (Ctrl_branch_taken && config->branchPredictor && config->branchTargetBuffer)
+      return true;
+
+    return false;
+  };
+
+  while (doFetch() && Ctrl_IF && (int)IF.size() < config->pipelineWidth) {
     size_t size = trace_get_item(&tr_entry); /* put the instruction into a buffer */
     if (size > 0) {
       dinst.inst = *tr_entry;
       dinst.seq = cur_seq++;
       IF.push_back(dinst);
       insts++;
+
+      if (is_branch(dinst) && dinst.inst.Addr != dinst.inst.PC + 4) {
+        Ctrl_branch_taken = true;
+      }
     } else {
       break;
     }
+
     if (verbose) {/* print the instruction entering the pipeline if verbose=1 */
       printf("[%d: IF] %s\n", cycle_number, get_instruction_string(IF.back(), true));
     }
   }
+
+  if (Ctrl_branch_taken && !config->branchPredictor && !config->branchTargetBuffer) {
+    while ((int)IF.size() < config->pipelineWidth) {
+      IF.push_back(get_NOP());
+    }
+  }
+
   inst_number += insts;
   return insts;
 }
